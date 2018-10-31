9e6421ab6f8c14f11ca1777fac206096ac4c4371
Gabriel Brolo Tobar <gabrolo13@gmail.com>
2018-10-30 19:10:27 -0600

    Redux thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import starter from './reducers';
 import App from "./components/App.js";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(starter);
+const store = createStore(
+    starter,
+    applyMiddleware(thunk)
+);
 
 ReactDOM.render((
     <Provider store={store}>
